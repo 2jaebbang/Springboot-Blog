c938fcc65ac5c9d38a60709bb4ca169af52ba26f
jaeyoung Lee <2jaebbang@naver.com>
2021-05-26 15:22:23 +0900

    회원수정

--- src/main/java/com/cos/blog/config/auth/PrincipalDetail.java
+++ src/main/java/com/cos/blog/config/auth/PrincipalDetail.java
@@ -4,67 +4,52 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.cos.blog.model.User;
 
+import lombok.Data;
 import lombok.Getter;
 
-//principal (접근 주체) = 세션처럼 사용 = Spring Security Context 에 보관됨
-//스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails 타입의 오브젝트를
-//스프링 시큐리티의 고유한 세션저장소에 저장을 해준다. 
-
-@Getter
+//  스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails 타입의 오브젝트를
+// 스프링 시큐리티의 고유한 세션저장소에 저장을 해준다.
+@Data
 public class PrincipalDetail implements UserDetails{
+	private User user; // 콤포지션
 
-	private static final long serialVersionUID = 7645618956884452156L;
-	
 	public PrincipalDetail(User user) {
 		this.user = user;
 	}
 	
-	private User user;
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	// 계정의 비밀번호를 리턴한다.
 	@Override
 	public String getPassword() {
 		return user.getPassword();
 	}
 
-	// 계정의 이름을 리턴한다.
 	@Override
 	public String getUsername() {
 		return user.getUsername();
 	}
 
-	// 계정이 만료되지 않았는 지 리턴한다. (true: 만료안됨)
+	// 계정이 만료되지 않았는지 리턴한다. (true: 만료안됨)
 	@Override
 	public boolean isAccountNonExpired() {
 		return true;
 	}
 
-	// 계정이 잠겨있지 않았는 지 리턴한다. (true: 잠기지 않음)
+	// 계정이 잠겨있지 않았는지 리턴한다. (true: 잠기지 않음)
 	@Override
 	public boolean isAccountNonLocked() {
 		return true;
 	}
 
-	// 비밀번호가 만료되지 않았는 지 리턴한다. (true: 만료안됨)
+	// 비밀번호가 만료되지 않았는지 리턴한다. (true: 만료안됨)
 	@Override
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
 
-	// 계정이 활성화(사용가능)인 지 리턴한다. (true: 활성화)
+	// 계정이 활성화(사용가능)인지 리턴한다. (true: 활성화)
 	@Override
 	public boolean isEnabled() {
 		return true;
@@ -74,9 +59,9 @@ public class PrincipalDetail implements UserDetails{
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		
-		Collection<SimpleGrantedAuthority> collectors = new ArrayList<>();
-		collectors.add(new SimpleGrantedAuthority("ROLE_"+user.getRole()));
-				
+		Collection<GrantedAuthority> collectors = new ArrayList<>();
+		collectors.add(()->{ return "ROLE_"+user.getRole();});
+		
 		return collectors;
 	}
 	
