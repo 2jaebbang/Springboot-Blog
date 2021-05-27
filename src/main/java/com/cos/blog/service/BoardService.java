004472ee825dd68b404b915e1ed98e7db07d6f30
jaeyoung Lee <2jaebbang@naver.com>
2021-05-27 15:34:34 +0900

    댓글작성(dto X)

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -10,16 +10,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.model.Board;
+import com.cos.blog.model.Reply;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
+import com.cos.blog.repository.ReplyRepository;
 import com.cos.blog.repository.UserRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class BoardService {
 
-	@Autowired
-	private BoardRepository boardRepository;
+	private final BoardRepository boardRepository;
+	private final ReplyRepository replyRepository;
 
 	@Transactional
 	public void 글쓰기(Board board, User user) {
@@ -54,5 +59,22 @@ public class BoardService {
 		board.setTitle(requestBoard.getTitle());
 		board.setContent(requestBoard.getContent());
 		// 해당 함수로 종료시 ( Service가 종료될때) 트랜잭션이 종료됨. 더티체킹 - 자동 업데이트 일어남. db flush
+	}
+	
+	@Transactional
+	public void 댓글쓰기(User user, int boardId, Reply requestReply) {
+		Board board = boardRepository.findById(boardId).orElseThrow(() -> {
+			return new IllegalArgumentException("댓글 쓰기 실패: 게시글 id를 찾을 수 없습니다.");
+		});
+		
+		requestReply.setUser(user);
+		requestReply.setBoard(board);
+		
+		replyRepository.save(requestReply);
+	}
+	
+	@Transactional
+	public void 댓글삭제(int replyId) {
+		replyRepository.deleteById(replyId);
 	}
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,79 +1,109 @@
 let index = {
-	init: function() {
-		$("#btn-save").on("click", () => {
-			this.save();
-		});
-		$("#btn-delete").on("click", () => {
-			this.deleteById();
-		});
-		$("#btn-update").on("click", () => {
-			this.update();
-		});
-	},
+		init: function(){
+			$("#btn-save").on("click", ()=>{ 
+				this.save();
+			});
+			$("#btn-delete").on("click", ()=>{ 
+				this.deleteById();
+			});
+			$("#btn-update").on("click", ()=>{ 
+				this.update();
+			});
+			$("#btn-reply-save").on("click", ()=>{ 
+				this.replySave();
+			});
+		},
 
-	save: function() {
+		save: function(){
+			let data = {
+					title: $("#title").val(),
+					content: $("#content").val()
+			};
+			
+			$.ajax({ 
+				type: "POST",
+				url: "/api/board",
+				data: JSON.stringify(data),
+				contentType: "application/json; charset=utf-8",
+				dataType: "json"
+			}).done(function(resp){
+				alert("글쓰기가 완료되었습니다.");
+				location.href = "/";
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			}); 
+		},
 		
-		let data = {
-			title: $("#title").val(),
-			content: $("#content").val()
-		};
+		deleteById: function(){
+			let id = $("#id").text();
+			
+			$.ajax({ 
+				type: "DELETE",
+				url: "/api/board/"+id,
+				dataType: "json"
+			}).done(function(resp){
+				alert("삭제가 완료되었습니다.");
+				location.href = "/";
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			}); 
+		},
 		
-		$.ajax({
-			type: "POST",
-			url: "/api/board",
-			data: JSON.stringify(data),
-			contentType: "application/json; charsey=utf-8",
-			dataType: "json"  
+		update: function(){
+			let id = $("#id").val();
+			
+			let data = {
+					title: $("#title").val(),
+					content: $("#content").val()
+			};
 
-		}).done(function(resp) {
-			alert("글 쓰기가 완료되었습니다.");
-			location.href = "/";
-		}).fail(function(error) {
-			alert(JSON.stringify(error));
-		});
-	},
-	
-	deleteById: function() {
-		let id = $("#id").text();
+			$.ajax({ 
+				type: "PUT",
+				url: "/api/board/"+id,
+				data: JSON.stringify(data),
+				contentType: "application/json; charset=utf-8",
+				dataType: "json"
+			}).done(function(resp){
+				alert("글수정이 완료되었습니다.");
+				location.href = "/";
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			}); 
+		},
 		
-		$.ajax({
-			type: "DELETE",
-			url: "/api/board/"+id, 
-			dataType: "json"
+		replySave: function(){
+			let data = {
+					userId: $("#userId").val(),
+					boardId: $("#boardId").val(),
+					content: $("#reply-content").val()
+			};
+			
+			$.ajax({ 
+				type: "POST",
+				url: `/api/board/${data.boardId}/reply`,
+				data: JSON.stringify(data),
+				contentType: "application/json; charset=utf-8",
+				dataType: "json"
+			}).done(function(resp){
+				alert("댓글작성이 완료되었습니다.");
+				location.href = `/board/${data.boardId}`;
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			}); 
+		},
 		
-		}).done(function(resp) {
-			alert("삭제가 완료되었습니다.");
-			location.href="/";
-		}).fail(function(error) {
-			alert(JSON.stringify(error));
-		});
-	},
-	
-	update: function() {
-		let id = $("#id").val();
-		
-		let data = {
-			title: $("#title").val(),
-			content: $("#content").val()
-		};
-		
-	
-		
-		$.ajax({
-			type: "PUT",
-			url: "/api/board/"+id,
-			data: JSON.stringify(data),
-			contentType: "application/json; charsey=utf-8",
-			dataType: "json"  
-
-		}).done(function(resp) {
-			alert("글 수정이 완료되었습니다.");
-			location.href = "/";
-		}).fail(function(error) {
-			alert(JSON.stringify(error));
-		});
-	}
-	
+		replyDelete : function(boardId, replyId){
+			$.ajax({ 
+				type: "DELETE",
+				url: `/api/board/${boardId}/reply/${replyId}`,
+				dataType: "json"
+			}).done(function(resp){
+				alert("댓글삭제 성공");
+				location.href = `/board/${boardId}`;
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			}); 
+		},
 }
 
 index.init();
